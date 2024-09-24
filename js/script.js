faf5f79c4cdf7b320ca3b896cfe80dd2946247ef
Darmadani Kyat <darmadanikm@gmail.com>
2024-09-25 03:51:20 +0700

    form commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 function validateForm(){
-    const name = document.forms["message-form"]["full-name"].value
-    const birthdate = document.forms["message-form"]["birth-date"].value
-    const gender = document.forms["message-form"]["gender"].value
-    const pesan = document.forms["message-form"]["pesan"].value
+    const name = document.forms["message-form"]["full-name"].value;
+    const birthdate = document.forms["message-form"]["birth-date"].value;
+    const gender = document.forms["message-form"]["gender"].value;
+    const pesan = document.forms["message-form"]["pesan"].value;
 
     if(name == "" || birthdate == "" || gender == "" || pesan == ""){
         alert("Tidak boleh ada yang kosong");
-        return false
+        return false;
     }
 
     setSendUI(name, birthdate, gender, pesan);
@@ -15,6 +15,7 @@ function validateForm(){
 }
 
 function setSendUI(name, birthdate, gender, pesan){
+    document.getElementById("send-time").innerHTML = new Date();
     document.getElementById("send-fullname").innerHTML = name;
     document.getElementById("send-birthdate").innerHTML = birthdate;
     document.getElementById("send-gender").innerHTML = gender;
