130859591d1803b724b814858cc93b0bbcf9fd3f
Darmadani Kyat <darmadanikm@gmail.com>
2024-10-02 16:14:34 +0700

    form commit with table

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
+// function replaceName(){
+//     let name = prompt("Who are you?", "Enter your name");
+//     document.getElementById("welcome-name").innerHTML = name;
+// }
+
+// replaceName();
+
 function validateForm(){
     const name = document.forms["message-form"]["full-name"].value;
     const birthdate = document.forms["message-form"]["birth-date"].value;
     const gender = document.forms["message-form"]["gender"].value;
     const pesan = document.forms["message-form"]["pesan"].value;
 
-    if(name == "" || birthdate == "" || gender == "" || pesan == ""){
-        alert("Tidak boleh ada yang kosong");
-        return false;
+    // if(name == "" || birthdate == "" || gender == "" || pesan == ""){
+    //     alert("Tidak boleh ada yang kosong");
+    //     return false;
+    // }
+
+    if(name == ""){
+        document.getElementById("error-name").innerHTML = "*Please input name first"
     }
 
     setSendUI(name, birthdate, gender, pesan);
@@ -21,3 +32,4 @@ function setSendUI(name, birthdate, gender, pesan){
     document.getElementById("send-gender").innerHTML = gender;
     document.getElementById("send-pesan").innerHTML = pesan;
 }
+
